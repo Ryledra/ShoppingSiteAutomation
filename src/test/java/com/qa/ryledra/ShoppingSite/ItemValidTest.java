e3b830cfa0339c5dfa96818067e7e1ec54052cd2
Ryledra <vince@nadin.one>
2018-09-18 12:05:21 +0100

    parameterisation added to test: 2 passes and 1 fail (shortshorts not on page

--- src/test/java/com/qa/ryledra/ShoppingSite/ItemValidTest.java
+++ src/test/java/com/qa/ryledra/ShoppingSite/ItemValidTest.java
@@ -2,19 +2,38 @@ package com.qa.ryledra.ShoppingSite;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+@RunWith(Parameterized.class)
 public class ItemValidTest {
+	
+	@Parameters
+	public static List<String> data() {
+		return Arrays.asList(
+				new String[] {"Blouse", "Printed Dress", "Short Shorts"});
+	}
+	
+	public ItemValidTest(String item)	{
+		searchItem = item;
+	}
 	
 	WebDriver driver;
 	String url = "http://automationpractice.com/index.php";
 	final String cDFilePath = "C:\\Testing\\chromedriver.exe";
 	final String driverType = "webdriver.chrome.driver";
+	
+	private String searchItem;
 	
 	@Before
 	public void setup()	{
@@ -27,8 +46,6 @@ public class ItemValidTest {
 	@Test
 	public void testAssertItem()	{
 		
-		String searchItem = "Blouse";
-		
 		Index indexPage = PageFactory.initElements(driver, Index.class);
 		indexPage.moveToWomen();
 		
@@ -38,7 +55,7 @@ public class ItemValidTest {
 	
 	@After
 	public void tearDown() throws InterruptedException	{
-		Thread.sleep(3000);
+		// Thread.sleep(3000);
 		driver.quit();
 	}
 }
---
+++ src/test/java/com/qa/ryledra/ShoppingSite/TestSuite.java
@@ -0,0 +1,11 @@
+package com.qa.ryledra.ShoppingSite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ItemValidTest.class
+})
+
+public class TestSuite {}
